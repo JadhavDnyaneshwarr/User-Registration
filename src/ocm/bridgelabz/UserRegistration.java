0c82bac4d73e985033d924fb1a5e1c988f2c4fc2
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-02-06 14:00:41 +0530

    Welcome to User Registration Program

---
+++ src/ocm/bridgelabz/UserRegistration.java
@@ -0,0 +1,7 @@
+package ocm.bridgelabz;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("welcome to User Registration Problem");
+    }
+}
