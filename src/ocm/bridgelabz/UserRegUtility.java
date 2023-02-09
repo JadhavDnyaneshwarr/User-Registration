50da10a2c27be44b8814c4329ce29121f2496af0
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-02-09 17:26:42 +0530

    Added: Entering Email

--- src/ocm/bridgelabz/UserRegUtility.java
+++ src/ocm/bridgelabz/UserRegUtility.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class UserRegUtility {
     static boolean firstName(String firstName){
-        String validFirstName = ("^[A-Z]?[a-zA-z]{2,}$");
+        String validFirstName = ("^[A-Z]?[a-zA-Z]{2,}$");
         Pattern pattern = Pattern.compile(validFirstName);
         Matcher matcher = pattern.matcher(firstName);
         if (matcher.matches()){
@@ -17,14 +17,26 @@ public class UserRegUtility {
         }
     }
     static boolean lastName(String lastName){
-        String validFirstName = ("^[A-Z]?[a-zA-z]{2,}$");
-        Pattern pattern = Pattern.compile(validFirstName);
+        String validLastName = ("^[A-Z]?[a-zA-Z]{2,}$");
+        Pattern pattern = Pattern.compile(validLastName);
         Matcher matcher = pattern.matcher(lastName);
         if (matcher.matches()){
             System.out.println("Last Name is Valid");
             return true;
         }else {
             System.out.println("Last Name is Invalid");
+            return false;
+        }
+    }
+    static boolean email(String email){
+        String validEmail = ("^[a-z]{3,}[.]?([x-z]*)+@([b][l][.][c][o])+([.][i]?[n]?)$");
+        Pattern pattern = Pattern.compile(validEmail);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches()){
+            System.out.println("Email is Valid");
+            return true;
+        }else {
+            System.out.println("Email is Invalid");
             return false;
         }
     }
