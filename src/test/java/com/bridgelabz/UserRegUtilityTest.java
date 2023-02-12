5d68c56fc3e52266aea9454b654b7b835fdeae9e
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-02-12 16:46:24 +0530

    Added: Junit Parametrised Test to Validate multiple entry for the Email Address

--- src/test/java/com/bridgelabz/UserRegUtilityTest.java
+++ src/test/java/com/bridgelabz/UserRegUtilityTest.java
@@ -4,9 +4,21 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+@RunWith(Parameterized.class)
 public class UserRegUtilityTest {
     UserRegUtility userReg;
+    private boolean expected;
+    private String email;
+
+    public UserRegUtilityTest(boolean expected, String email){
+        this.expected = expected;
+        this.email=email;
+    }
 
     @Before
     public void setup(){
@@ -37,5 +49,14 @@ public class UserRegUtilityTest {
     public void whenGivenValidPasswordShouldReturnTrue(){
         boolean validPassword = userReg.password("Dj@12345");
         Assertions.assertEquals(true, validPassword);
+    }
+    @Parameterized.Parameters
+    public static Collection data(){
+        Object[][] sampleEmails = new Object[][]{{true, "abc@yahoo.com"}, {true, "abc-100@yahoo.com"},{true,"abc.100@yahoo.com"}, {true, "abc111@abc.com"}, {true, "abc-100@abc.net"}, {true, "abc.100@abc.com.au"}, {true, "abc@1.com"}, {true, "abc@gmail.com.com"}, {true, "abc+100@gmail.com"}};
+        return Arrays.asList(sampleEmails);
+    }
+    @Test
+    public void WhenGivenSampleEmailShouldReturnTrue(){
+        Assertions.assertEquals(expected, userReg.sampleEmails(email));
     }
 }
